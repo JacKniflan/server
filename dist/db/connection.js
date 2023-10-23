5e8398d6d9f2ecbf6bb260cc731648533aadf5d9
Fabian <fabian.lillomartinez@gmail.com>
2023-10-22 21:43:22 -0300

    cambios codigo

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -2,6 +2,8 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
+//Object.defineProperty no sirve para definir una propiedad de un objeto 
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
 const keys_1 = __importDefault(require("../keys"));
